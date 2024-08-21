0c899320bec86d3a2497250ed78ed273d86ef5d0
FaisaOnGit <faisaloktovian1@gmail.com>
2024-08-21 12:07:33 +0700

    push

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-red-600">Home Page</h1>
+      <a href="/about">To about page</a>
+    </div>
+  );
+}
+
+export default Home;
